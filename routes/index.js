3c8c9a61efc9dc164d5aefa4f72eb0fbb1af761d
Omar Zukanović <omarzukanovic0@gmail.com>
2023-05-21 14:15:49 +0200

    implemented caching

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,29 @@
 var express = require("express");
 var router = express.Router();
 const app = express();
+const NodeCache = require('node-cache');
+
+const cache = new NodeCache();
+app.get('/api/data', (req, res) => {
+  const cacheKey = 'data'; // Key for caching
+
+// Check if data exists in cache
+const cachedData = cache.get(cacheKey);
+if (cachedData) {
+  return res.json(cachedData);
+}
+
+const data = fetchDataFromSource();
+// Store data in cache with a specified TTL (time to live)
+cache.set(cacheKey, data, 300
+  );
+
+  return res.json(data);
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
