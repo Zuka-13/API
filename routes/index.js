137f1a185a9806d6e9f24e56e7327504d211062c
Omar Zukanović <omarzukanovic0@gmail.com>
2023-05-21 13:59:04 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var router = express.Router();
+const app = express();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
+
+// Include Swagger documentation setup
+require('./swagger')(app);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,3 +7,22 @@ router.get("/", function (req, res, next) {
 });
 
 module.exports = router;
+
+/**
+* @swagger
+* /users:
+*   get:
+*     summary: Get all users
+*     description: Retrieve a list of all users.
+*     responses:
+*       200:
+*         description: Successful operation
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Users'
+*/
+app.get('/users', (req, res) => {
+ // Your route implementation
+ res.json(users);
+});
